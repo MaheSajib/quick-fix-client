b2a6b53cc7ef80ec67337eb1f029bcd9acc2f04c
MaheSajib <mahe.sajib@gmail.com>
2021-04-20 16:36:28 +0600

    Update Status

--- src/components/Dashboard/OrderList/OrderList.js
+++ src/components/Dashboard/OrderList/OrderList.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const OrderList = () => {
+
+
     const [orders, setOrders] = useState([])
-    fetch('https://shielded-spire-18150.herokuapp.com/orderList')
+    
+    useEffect(()=>{
+        fetch('https://shielded-spire-18150.herokuapp.com/orderList')
         .then(res => res.json())
         .then(data => setOrders(data))
+
+    }, [])
+
+        const handleStatus = (event, id) => {
+            const status = event.target.value;
+            const url = `https://shielded-spire-18150.herokuapp.com/processStatus?status=${status}&id=${id}`;
+            fetch(url , {
+                method: 'PATCH'
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+        }
+
     return (
         <div>
             <h1>Order List</h1>
@@ -14,16 +31,23 @@ const OrderList = () => {
                         <th scope="col">Name</th>
                         <th scope="col">Email</th>
                         <th scope="col">Service</th>
+                        <th scope="col">Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        orders.map(order => 
-                        <tr>
-                            <td>{order.name}</td>
-                            <td>{order.email}</td>
-                            <td>{order.service}</td>
-                        </tr>)
+                        orders.map(order =>
+                            <tr>
+                                <td>{order.name}</td>
+                                <td>{order.email}</td>
+                                <td>{order.service}</td>
+                                <td> <select onChange={(event) => handleStatus(event , order._id)} id="inputState" class="form-control">
+                                    <option selected>{order.status}</option>
+                                    <option>Pending</option>
+                                    <option>On Going</option>
+                                    <option>Done</option>
+                                </select></td>
+                            </tr>)
                     }
                 </tbody>
             </table>
